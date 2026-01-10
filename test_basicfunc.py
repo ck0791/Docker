38e5c16be41bae16dc748a10da29056fa4d2b322
ck0791 <163730073+ck0791@users.noreply.github.com>
2026-01-10 11:27:17 +0100

    Add setup and teardown for test variables

--- test_basicfunc.py
+++ test_basicfunc.py
@@ -4,16 +4,18 @@ from basicfunc import add, subtract
 def setup_function(function):
     print(f" Running Setup: {function.__name__}")
     function.x = 10
+    function.y = 2
 
 
 def teardown_function(function):
     print(f" Running Teardown: {function.__name__}")
     del function.x
+    del function.y
 
 
 ### Run to see failed test
 #def test_basicfunc_add():
-#    assert add(test_basicfunc_add.x) == 12
+#    assert add(test_basicfunc_add.x + test_basicfunc_subtract.y) == 11
 
 def test_basicfunc_subtract():
-    assert subtract(test_basicfunc_subtract.x) == 9
+    assert subtract(test_basicfunc_subtract.x - test_basicfunc_subtract.y) == 8
