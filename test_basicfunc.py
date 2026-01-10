9db284e770095ef4a97eeb515055f8070950088d
ck0791 <163730073+ck0791@users.noreply.github.com>
2026-01-10 11:37:48 +0100

    Refactor test setup and remove redundant code

--- test_basicfunc.py
+++ test_basicfunc.py
@@ -1,21 +1,10 @@
 from basicfunc import add, subtract
 
-
-def setup_function(function):
-    print(f" Running Setup: {function.__name__}")
-    function.x = 10
-    function.y = 2
-
-
-def teardown_function(function):
-    print(f" Running Teardown: {function.__name__}")
-    del function.x
-    del function.y
-
-
+x = 10
+y = 2
 ### Run to see failed test
 #def test_basicfunc_add():
-#    assert add(test_basicfunc_add.x + test_basicfunc_subtract.y) == 11
+#    assert add(x,y) == 11
 
 def test_basicfunc_subtract():
-    assert subtract(function.x,function.y) == 8
+    assert subtract(x,y) == 8
